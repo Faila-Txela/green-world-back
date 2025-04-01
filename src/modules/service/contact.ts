72ef06fab4f876e2f9d01805aedabbf780d76026
Faila Txela <129459141+Faila-Txela@users.noreply.github.com>
2025-04-01 19:49:12 +0100

    the main.

--- src/modules/service/contact.ts
+++ src/modules/service/contact.ts
@@ -1,3 +1,4 @@
+import { FastifyRequest, FastifyReply } from "fastify";
 import { contactoModel } from "../models/contact";
 import { contactoValidation } from "../validations/contact";
 import { BaseService } from "./base";
@@ -6,6 +7,20 @@ class ContactoService extends BaseService {
     model = contactoModel;
     createValidationSchema = contactoValidation.getData;
     updateValidationSchema = contactoValidation.getDataToUpdate;
+
+    async create(req: FastifyRequest, res: FastifyReply) {
+        try {
+            
+        const { nome, mensagem, userId, email } = contactoValidation.getData.parse(req.body)
+        const contacto = await contactoModel.create({ nome, mensagem, userId, email });
+        return res.status(201).send(contacto);
+
+        } catch (error) {
+            console.error(error);
+            return res.status(400).send({ message: error });
+        }
+    }
+
 }
 
 export const contactoService = new ContactoService();
