853931e4bcc2f0208c6d3fa74b1af5aee813e8ef
Faila Txela <129459141+Faila-Txela@users.noreply.github.com>
2025-05-29 22:29:52 +0100

    new alteration

--- src/modules/models/notificacao.ts
+++ src/modules/models/notificacao.ts
@@ -6,44 +6,44 @@ class NotificacaoModel extends BaseModel<Notificacao> {
     model = prisma.notificacao;
     include = {}
 
-    async getByEmpresaId(empresaId: string){
-       return await this.model.findMany({
-          where:{
-             empresaId
-           }
-      })
-    }
+   //  async getByEmpresaId(empresaId: string){
+   //     return await this.model.findMany({
+   //        where:{
+   //           empresaId
+   //         }
+   //    })
+   //  }
 
-// async getByEmpresaId(empresaId: string, lida?: boolean) {
-//     return await this.model.findMany({
-//         where: {
-//             empresaId,
-//             ...(lida !== undefined && { lida })
-//         },
-//         orderBy: {
-//             createAt: 'desc' // Ordena por data decrescente
-//         }
-//     });
-// }
+async getByEmpresaId(empresaId: string, lida?: boolean) {
+    return await this.model.findMany({
+        where: {
+            empresaId,
+            ...(lida !== undefined && { lida })
+        },
+        orderBy: {
+            createAt: 'desc' // Ordena por data decrescente
+        }
+    });
+}
 
-// async getByUserId(userId: string, lida?: boolean) {
-//     return await this.model.findMany({
-//         where: {
-//             userId,
-//             ...(lida !== undefined && { lida })
-//         },
-//         orderBy: {
-//             createAt: 'desc' // Ordena por data decrescente
-//         }
-//     });
-// }
+async getByUserId(userId: string, lida?: boolean) {
+    return await this.model.findMany({
+        where: {
+            userId,
+            ...(lida !== undefined && { lida })
+        },
+        orderBy: {
+            createAt: 'desc' // Ordena por data decrescente
+        }
+    });
+}
 
-// async updateLidaStatus(id: string, lida: boolean) {
-//     return await this.model.update({
-//         where: { id },
-//         data: { lida }
-//     });
-// }
+async updateLidaStatus(id: string, lida: boolean) {
+    return await this.model.update({
+        where: { id },
+        data: { lida }
+    });
+}
 
 }
 
