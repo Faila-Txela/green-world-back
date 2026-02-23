95edb65feecd1b385467e5f4e41ba004cd954299
Faila Txela <129459141+Faila-Txela@users.noreply.github.com>
2026-02-23 19:55:54 +0100

    news update

--- src/routes/pontos.routes.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance } from "fastify";
-import { BaseRoute } from "./base";
-import { pontosService } from "../modules/service/pontos";
-
-export async function pontos(app: FastifyInstance) {
-    await BaseRoute.handle(app, pontosService, 'pontos');
-    
-}
